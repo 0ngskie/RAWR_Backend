3520960b3866b1f457beb5cc9f1aacad78aa5bc3
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-05 22:41:40 +0800

    Update in Shops
    Made some changes in the shop section of the Repository LoL

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,6 +1,7 @@
 const Shop = require('models/shops');
 const mysqlConnection = require('mysql/mysqlConnection');
 
+
 // Create
 module.exports.registerRepairShop = (req, res) => {
     const {shop_Name, shop_Address, shop_Document_Filepath} = req.body;
@@ -21,28 +22,65 @@ module.exports.registerRepairShop = (req, res) => {
 
 // Read
 
-module.exports.getRepairShops = (req, res) => {
-    const{shop_Name,shop_Address, description, shop_Mobile_No, shop_Landline_No, shop_Page_Payment_Method, shop_Page_Registration_Status} = req.body;
-    const query = `SELECT * FROM repairshop`;
-    const values = [shop_Name, shop_Address, description, shop_Mobile_No, shop_Landline_No, shop_Page_Payment_Method, shop_Page_Registration_Status];
+module.exports. locateRepairShop = (req,res) => {
+    const{shop_Name, shop_Address,shop_Coordinates,shop_Page_Payment_Method} = req.body;
+    const query = `SELECT shop_Name, shop_Address, shop_Coordinates FROM shop WHERE shop_Name = ? OR shop_Address = ? OR shop_Page_Payment_Method = ?`;
+    const values = [shop_Name, shop_Address, shop_Coordinates, shop_Page_Payment_Method, req.params.shop_Name, req.params.shop_Address, req.params.shop_Page_Payment_Method];
 
-    mysqlConnection.execute(query,values, (error, result) => {
+    mysqlConnection.execute(query, values, (error, result) => {
         if(error){
-            console.error('Error getting repairshops:', error);
-            return res.status(500).json({error: 'Error getting repairshops'});
+            console.error('Error locating repairshop:', error);
+            return res.status(500).json({error: 'Error locating repairshop'});
+        
         }
+        res.status(200).json({message : 'Shop found:'});
         res.status(200).json(result);
+        
     });
+};
+
+module.exports.displayShopPage = (req, res) => {
+    const{shop_Page_Status} = req.body;
+    const checkQuery =`SELECT shop_Page_Status FROM shops WHERE shop_Name = ? OR shop_Address = ?`;
+    const checkValues = [shop_Page_Status, req.params.shop_Name, req.params.shop_Address];
+
+    mysqlConnection.execute(checkQuery, checkValues, (checkError,checkResult) =>{
+        if(checkError){
+            console.error('Error getting shop status:', error);
+            return res.status(500).json({error: 'Error getting shop status from server'});
+        }
+        if(checkResult.length > 0){
+            res.status(200).json({message: 'Shop Page status is found and dispatched successfully'});
+            res.status(200).json({result: checkResult});
+
+            //TODO find a way to display Comment (bro... just use JOIN LOL) (Thanks Personality 2 <3)
+            const{shop_Name,shop_Address, shop_AboutUs,shop_Service_Offer, shop_Mobile_No, shop_Landline_No,shop_Picture_Filepath, shop_Page_Payment_Method} = req.body;
+            const query = `SELECT shop_Name, shop_Address, shop_About_Us, shop_Service_Offer, shop_Mobile_No, shop_Picture_Filepath, shop_Page_Payment_Method FROM shops`;
+            const values = [shop_Name, shop_Address, shop_AboutUs, shop_Service_Offer, shop_Mobile_No, shop_Landline_No, shop_Picture_Filepath, shop_Page_Payment_Method];
+
+            mysqlConnection.execute(query,values, (error, result) => {
+                if(error){
+                    console.error('Error getting repairshops:', error);
+                    return res.status(500).json({error: 'Error getting shop details from server'});
+                }
+                res.status(200).json({message: 'Shop Page is found and dispatched successfully'});
+                res.status(200).json({result: result});
+                
+            });
+        }
+        
+    });    
 };
 
 // Update
 
-module.exports.updateRepairShop = (req, res) => {
-    const {shop_Name, shop_Address, shop_Email, shop_Mobile_No, shop_Landline_No, shop_Coordinates, shop_Description} = req.body;
 
-    const query = `UPDATE shop SET shop_Name = ?, shop_Address = ?, shop_Email = ?, shop_Mobile_Num = ?, shop_Landline_Num = ?, shop_Coordinates = ?, shop_Description = ? WHERE shop_id = ?`;
+module.exports.updateShopPage = (req, res) => {
+    const {shop_Name, shop_Address, shop_Email, shop_Mobile_No, shop_Landline_No, shop_Description} = req.body;
 
-    const values = [shop_Name, shop_Address, shop_Email, shop_Mobile_No,shop_Landline_No,shop_Coordinates,shop_Description, req.params.id];
+    const query = `UPDATE shop SET shop_Name = ?, shop_Address = ?, shop_Email = ?, shop_Mobile_No = ?, shop_Landline_No = ?, shop_Coordinates = ?, shop_Description = ? WHERE shop_id = ?`;
+
+    const values = [shop_Name, shop_Address, shop_Email, shop_Mobile_No,shop_Landline_No,shop_Description, req.params.id];
     mysqlCon.query(query, values, (error, result) => {
         if(error){
             console.error('Error updating repairshop:', error);
@@ -55,11 +93,12 @@ module.exports.updateRepairShop = (req, res) => {
 
 // Delete
 
+// Under Progress
 module.exports.deleteRepairShop = (req, res) => {
     const query = `DELETE FROM shop WHERE shop_id = ?`;
 
     const values = [req.params.id];
-    mysqlCon.query(query, values, (error, result) => {
+    mysqlConnection.execute(query, values, (error, result) => {
         if(error){
             console.error('Error deleting repairshop:', error);
             return res.status(500).json({error: 'Error deleting repairshop'});
--- models/shops.js
+++ models/shops.js
@@ -1,20 +1,23 @@
 class Shop{
     constructor(shop_id, shopName, 
                 shop_Email,shop_Address,
-                shop_Coordinates,shop_Description,
-                shop_Mobile_No,shop_Landline_No, 
-                shop_Document_Filepath,shop_Page_Payment_Method,
-                shop_Page_Status,shop_Registration_Status)
+                shop_Coordinates,shop_AboutUs,
+                shop_Service_Offer,shop_Mobile_No,
+                shop_Landline_No, shop_Document_Filepath,
+                shop_Profile_Filepath, shop_Page_Payment_Method, 
+                shop_Page_Status, shop_Registration_Status)
                 {
                     this.shop_id = shop_id;
                     this.shopName = shopName;
                     this.shop_Email = shop_Email;
                     this.shop_Address = shop_Address;
                     this.shop_Coordinates = shop_Coordinates;
-                    this.shop_Description = shop_Description;
+                    this.shop_AboutUs = shop_AboutUs;
+                    this.shop_Service_Offer = shop_Service_Offer;
                     this.shop_Mobile_No = shop_Mobile_No;
                     this.shop_Landline_No = shop_Landline_No;
                     this.shop_Document_Filepath = shop_Document_Filepath;
+                    this.shop_Profile_Filepath = shop_Profile_Filepath;
                     this.shop_Page_Payment_Method = shop_Page_Payment_Method;
                     this.shop_Page_Status = shop_Page_Status;
                     this.shop_Registration_Status = shop_Registration_Status;
