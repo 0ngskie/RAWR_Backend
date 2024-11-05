f13a3dd4a0aa0df87498bd9fea2a8c85ad864dee
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-05 16:45:25 +0800

    Updates Account Controller.js
    Finished the needed functionalities for the Controller

---
+++ controllers/accountController.js
@@ -0,0 +1,87 @@
+const User = require("../models/users")
+const mysqlConnection = require('../mysql/mysqlConnection')
+
+// this module checks the Users password  (Under review whether to keep or disregard)
+module.exports.checkPassword = (req,res) =>{
+    const {password} = req.body;
+    const checkQuery = `SELECT * FROM users WHERE password =?`;
+    const checkValues = [password];
+    mysqlConnection.execute(checkQuery,checkValues, (checkError, checkResults) =>{
+        if(checkError){
+            console.error('Error checking password:', checkError);
+            return res.status(500).json({ error: 'Error checking password' });
+        } else{
+            end
+        }
+    })
+}
+// this module checks if the Email already exists in the Database 
+module.exports.checkEmailExist = (req,res) =>{
+    const {email} = req.body;
+    const checkQuery = `SELECT * FROM users WHERE email =?`;
+    const checkValues = [email];
+    mysqlConnection.execute(checkQuery,checkValues, (checkError, checkResults) =>{
+        if(checkError){
+            console.error('Error checking email existence:', checkError);
+            return res.status(500).json({ error: 'Error checking email existence' });
+        }
+        if(checkResults.length > 0){
+            // Email already exists
+            return res.status(400).json({ error: 'Email already exists' });
+        }
+        else{
+            end
+        }
+    })
+}
+// this module validates the Users login in the System ( UI end Login Page)
+module.exports.validateAccountLogin = (req,res) =>{
+    const {email, password} = req.body;
+    const checkQuery = `SELECT * FROM users WHERE email =? AND password =?`;
+    const checkValues = [email, password];
+
+    mysqlConnection.execute(checkQuery, checkValues, (checkError, checkResult) =>{
+        if(checkError){
+            console.error('Error checking account login:', checkError);
+            return res.status(500).json({ error: 'Error checking account login' });
+        }
+        if(checkResult.length > 0){
+            // Account login successful
+            return res.status(200).json({ message: 'Account login successful' });
+        }
+        else{
+            // Incorrect email or password
+            return res.status(401).json({ error: 'Incorrect email or password' });
+        }
+    })
+}
+
+// Update User account
+module.exports.editUserAccount = (req, res) =>{
+    const {last_Name, first_Name, email, contact_No, password} = req.body;
+    const query = `UPDATE user set last_Name = ?, first_Name = ?, email = ?, password = ? WHERE user_id = ?`;
+    const values = [last_Name, first_name, email, contact_No, password, req.params.id];
+
+    mysqlConnection.execute(query, values, (error, result) => {
+        if (error) {
+            console.error('Error updating customer description:', error);
+            return res.status(500).json({ error: 'Error updating customer account' });
+        }
+        res.status(200).json(result);
+        res.status(200).json({message: "Customer Account updated"});
+    })
+}
+
+module.exports.deleteUserAcount = (req,res) =>{
+    const query = `DELETE FROM user where user_id =?`;
+    const values = [req.params.id];
+
+    mysqlConnection.execute(query, values, (error, result) => {
+        if(error){
+            console.error ('Error deleting user:', error);
+            return res.status(500).json({error: 'Error deleting your account'});
+        }
+        res.status (200).json(result);
+        res.status(200).json({ message: ' User account has been deleted'});
+    })
+}
