32ee27d43400ece8b281a802e6c724804c6613d7
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-05 16:47:03 +0800

    Update shopController.js
    Made changes in shop controller

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,53 +1,57 @@
-const Shop = require('models/shops')
-const mysqlCon = require('mysql/mysqlCon')
+const Shop = require('models/shops');
+const mysqlConnection = require('mysql/mysqlConnection');
+
 // Create
+module.exports.registerRepairShop = (req, res) => {
+    const {shop_Name, shop_Address, shop_Document_Filepath} = req.body;
 
-module.exports.createRepairShop = (req, res) => {
-    const {shop_Name, shop_Address, shop_Email, shop_Mobile_Num, shop_Landline_Num, shop_Coordinates, shop_Description} = req.body;
-
-    const query = `INSERT INTO repairshop (shop_Name, shop_Address, shop_Email,shop_Mobile_Num,shop_Landline_Num,shop_Coordinates,shop_Description) 
-                   VALUES (?,?,?,?,?,?,?)`;
+    const query = `INSERT INTO repairshop (shop_Name, shop_Address, shop_Document_FilePath) 
+                   VALUES (?,?,?)`;
                    
-    const values = [shop_Name, shop_Address, shop_Email,shop_Mobile_Num,shop_Landline_Num,shop_Coordinates,shop_Description];
-    mysqlCon.query(query, values, (error, result) => {
+    const values = [shop_Name, shop_Address, shop_Document_Filepath];
+    mysqlConnection.execute(query, values, (error, result) => {
         if(error){
             console.error('Error creating repairshop:', error);
-            return res.status(500).json({error: 'Error creating repairshop'});
+            return res.status(500).json({error: 'Error registering repairshop'});
         }
-        res.status(200).json({message: 'Repairshop created'});
-    })
-}
+        res.status(200).json({result: result});
+        res.status(200).json({message: 'Repairshop is under review'});
+    });
+};
 
 // Read
 
 module.exports.getRepairShops = (req, res) => {
+    const{shop_Name,shop_Address, description, shop_Mobile_No, shop_Landline_No, shop_Page_Payment_Method, shop_Page_Registration_Status} = req.body;
     const query = `SELECT * FROM repairshop`;
+    const values = [shop_Name, shop_Address, description, shop_Mobile_No, shop_Landline_No, shop_Page_Payment_Method, shop_Page_Registration_Status];
 
-    mysqlCon.query(query, (error, result) => {
+    mysqlConnection.execute(query,values, (error, result) => {
         if(error){
             console.error('Error getting repairshops:', error);
             return res.status(500).json({error: 'Error getting repairshops'});
         }
         res.status(200).json(result);
-    })
-}
+    });
+};
 
 // Update
 
 module.exports.updateRepairShop = (req, res) => {
-    const {shop_Name, shop_Address, shop_Email, shop_Mobile_Num, shop_Landline_Num, shop_Coordinates, shop_Description} = req.body;
+    const {shop_Name, shop_Address, shop_Email, shop_Mobile_No, shop_Landline_No, shop_Coordinates, shop_Description} = req.body;
 
     const query = `UPDATE shop SET shop_Name = ?, shop_Address = ?, shop_Email = ?, shop_Mobile_Num = ?, shop_Landline_Num = ?, shop_Coordinates = ?, shop_Description = ? WHERE shop_id = ?`;
 
-    const values = [shop_Name, shop_Address, shop_Email, shop_Mobile_Num,shop_Landline_Num,shop_Coordinates,shop_Description, req.params.id];
+    const values = [shop_Name, shop_Address, shop_Email, shop_Mobile_No,shop_Landline_No,shop_Coordinates,shop_Description, req.params.id];
     mysqlCon.query(query, values, (error, result) => {
         if(error){
             console.error('Error updating repairshop:', error);
             return res.status(500).json({error: 'Error updating repairshop'});
         }
+        res.status(200).json(result);
         res.status(200).json({message: 'Repairshop updated'});
-    })
-}
+    });
+};
 
 // Delete
 
@@ -60,6 +64,7 @@ module.exports.deleteRepairShop = (req, res) => {
             console.error('Error deleting repairshop:', error);
             return res.status(500).json({error: 'Error deleting repairshop'});
         }
+        res.status(200).json({result: result});
         res.status(200).json({message: 'Repairshop deleted'});
-    })
-}
+    });
+};
