b06976e1da1a48660e2bdfdbf3c40b98d96ab79d
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-14 00:07:07 +0800

    Update customer rating
    I just changed description into comment
    By far this is the smallest update LOL

--- models/customerRating.js
+++ models/customerRating.js
@@ -1,8 +1,8 @@
 class CustomerRating{
-    constructor(customerRating_id,rating,rating_Description,user_id,shop_id){
+    constructor(customerRating_id,rating,rating_comment,user_id,shop_id){
         this.customerRating_id = customerRating_id;
         this.rating = rating;
-        this.rating_Description = rating_Description;
+        this.rating_comment = rating_comment;
         this.user_id = user_id;
         this.shop_id = shop_id;
     }
