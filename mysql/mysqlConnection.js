3ed77922216a04856ccd430197ebc84ea1757876
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-21 00:19:19 +0800

    Update mysqlConnection
    updated the mysql connection I have updated the connect function added a console log where it would indicate in the terminal if the System was able to connect with the database

--- mysql/mysqlConnection.js
+++ mysql/mysqlConnection.js
@@ -6,12 +6,16 @@ var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 });
 
 con.connect(function(err) {
-    if(err) throw err;
-    console.log("Succesfully connected to the database named: " + DB_DATABASE);
+    if(err) {
+        console.error('Error connecting to the database:', err.message);
+        return;
+    }
+    console.log("Succesfully connected to the database named: ", process.env.DB_DATABASE);
 });
 
 module.exports = con;
