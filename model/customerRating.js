0abab6f5ff2c4d40fdda6c11ce1937a60800ca4b
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-21 17:45:11 +0800

    Update customer Rating
    updated the customer rating from ratings into rating_value

--- model/customerRating.js
+++ model/customerRating.js
@@ -1,7 +1,7 @@
 class CustomerRating{
-    constructor(customerRating_id,rating,rating_comment,user_id,shop_id){
+    constructor(customerRating_id,rating_value,rating_comment,user_id,shop_id){
         this.customerRating_id = customerRating_id;
-        this.rating = rating;
+        this.rating_value = rating_value;
         this.rating_comment = rating_comment;
         this.user_id = user_id;
         this.shop_id = shop_id;
