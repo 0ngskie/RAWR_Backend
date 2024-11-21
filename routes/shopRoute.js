e39c8eb243e71160505955d6454f4a27211c4ee7
0ngskie <ongjohnbenedict2@gmail.com>
2024-11-21 17:49:29 +0800

    Update shopRoute
    just added space LoL

--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -3,13 +3,17 @@ const router = express.Router();
 const shopController = require('../controllers/shopController');
 
 router.post('/register', shopController.registerRepairShop);
+
 router.get('/viewShopDetails', shopController.viewRepairShopDetails);
 router.get('/viewPageStatus', shopController.validateShopPageStatus);
 router.get('/locate', shopController.locateRepairShop);
 router.get('/display', shopController.displayShopPage);
+router.get('/shopRating/:shop_id', shopController.getShopAverageRating);
+
 router.put('/updateRegisStatus', shopController.updateShopRegistrationStatus);
 router.put('/updateRegisApplication', shopController.updateShopRegistrationDetails);
 router.put('/update', shopController.updateShopPage);
+
 router.delete('/delete', shopController.deleteRepairShop);
 
 module.exports = router;
